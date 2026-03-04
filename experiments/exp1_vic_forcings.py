8016ac50ede70c2432c570447cb2f3666f8321c0
Kostas Andreadis <kandread@umass.edu>
2026-03-04 05:50:35 -0500

    Fixed typo for data_dir

--- experiments/exp1_vic_forcings.py
+++ experiments/exp1_vic_forcings.py
@@ -8,22 +8,22 @@ def main():
     args = config.parse_args()
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
-    with h5py.File(f"{args.datadir}/{args.forcing}.h5") as f:
+    with h5py.File(f"{args.data_dir}/{args.forcing}.h5") as f:
         startdate = f.attrs["tstart"]
         enddate = f.attrs["tend"]
         bids = [b.decode() for b in f["bids"][:]]
-    soilfile = f"{args.datadir}/vic/vic.nldas.mexico.soil.txt"
+    soilfile = f"{args.data_dir}/vic/vic.nldas.mexico.soil.txt"
     outfile = f"{output_dir}/vic_{args.forcing}_baseline_soil.txt"
     with open(outfile, "w") as fout:
         for bid in tqdm(bids):
-            line = vic.calibrate(bid, soilfile, args.forcing, startdate, enddate, datadir=args.datadir)
+            line = vic.calibrate(bid, soilfile, args.forcing, startdate, enddate, datadir=args.data_dir)
             fout.write(line)
-    mod, obs = vic.evaluate(bids, outfile, args.forcing, startdate, enddate, args.datadir)
+    mod, obs = vic.evaluate(bids, outfile, args.forcing, startdate, enddate, args.data_dir)
     mod.to_csv(f"{output_dir}/vic_{args.forcing}_train_predictions.csv")
     obs.to_csv(f"{output_dir}/vic_{args.forcing}_train_observations.csv")
     val_tstart = "2000-10-01"
     val_tend = "2008-09-30"
-    mod, obs = vic.evaluate(bids, outfile, args.forcing, val_tstart, val_tend, args.datadir)
+    mod, obs = vic.evaluate(bids, outfile, args.forcing, val_tstart, val_tend, args.data_dir)
     mod.to_csv(f"{output_dir}/vic_{args.forcing}_valid_predictions.csv")
     obs.to_csv(f"{output_dir}/vic_{args.forcing}_valid_observations.csv")
 
